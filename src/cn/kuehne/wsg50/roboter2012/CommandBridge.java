ce3b76e0fa1e5838d1190ac8ae8fd948d59f45d7
Thomas Kuehne <thomas@kuehne.cn>
2012-10-31 13:11:36 +0100

    added partial robot2012 bridges

---
+++ src/cn/kuehne/wsg50/roboter2012/CommandBridge.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2012, Thomas Kuehne <thomas@kuehne.cn>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package cn.kuehne.wsg50.roboter2012;
+
+import java.util.List;
+
+import cn.kuehne.wsg50.Acknowledge;
+import cn.kuehne.wsg50.Command;
+import cn.kuehne.wsg50.E;
+import cn.kuehne.wsg50.PacketBuilder;
+import cn.kuehne.wsg50.PacketID;
+import cn.kuehne.wsg50.Parameter;
+
+public class CommandBridge implements Command {
+	private Command command;
+	private List<Acknowledge> replies;
+
+	public CommandBridge() {
+	}
+
+	public final CommandBridge getCommand() {
+		return this;
+	}
+
+	@Override
+	public final byte getPacketID() {
+		if (command == null) {
+			throw new IllegalStateException("'command' is null");
+		}
+		return command.getPacketID();
+	}
+
+	public final Parameter[] getParameters() {
+		if (command == null) {
+			throw new IllegalStateException("'command' is null");
+		}
+		return command.getParameters();
+	}
+
+	public final List<Acknowledge> getReplies() {
+		return replies;
+	}
+
+	public final boolean isError() {
+		if (replies != null && 0 < replies.size()) {
+			final Acknowledge last = replies.get(replies.size());
+			final short status = last.getStatusCode();
+			return !(E.SUCCESS.getCode() == status || E.CMD_PENDING.getCode() == status);
+		}
+		return false;
+	}
+
+	public boolean isFinished() {
+		if (replies != null && 0 < replies.size()) {
+			final Acknowledge last = replies.get(replies.size());
+			final short status = last.getStatusCode();
+			return E.SUCCESS.getCode() == status;
+		}
+		return false;
+	}
+
+	public final boolean isStarted() {
+		return replies != null;
+	}
+
+	public final boolean isTouched() {
+		return command != null;
+	}
+
+	public final void setCommand(final Command newCommand) {
+		if (isStarted()) {
+			throw new IllegalStateException("command has already been encoded");
+		}
+		if (newCommand == null) {
+			throw new IllegalArgumentException("'newCommand' is null");
+		}
+		command = newCommand;
+	}
+
+	public final void setCommand(final String name) {
+		if (isStarted()) {
+			throw new IllegalStateException("command has already been encoded");
+		}
+		final PacketID id = PacketID.valueOf(name);
+		command = id.getCommand();
+	}
+
+	@Override
+	public final void setPayload(byte[] p) {
+		if (command == null) {
+			throw new IllegalStateException("'command' is null");
+		}
+		command.setPayload(p);
+	}
+
+	@Override
+	public final void writePayload(PacketBuilder builder) {
+		if (command == null) {
+			throw new IllegalStateException("'command' is null");
+		}
+		command.writePayload(builder);
+	}
+}
---
+++ src/cn/kuehne/wsg50/roboter2012/CommunicatorBridge.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2012, Thomas Kuehne <thomas@kuehne.cn>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cn.kuehne.wsg50.roboter2012;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import cn.kuehne.wsg50.Acknowledge;
+import cn.kuehne.wsg50.Command;
+import cn.kuehne.wsg50.Input;
+import cn.kuehne.wsg50.Output;
+import cn.kuehne.wsg50.PacketCoder;
+import cn.kuehne.wsg50.helper.InputFromStream;
+import cn.kuehne.wsg50.helper.OutputToStream;
+
+public class CommunicatorBridge implements Closeable {
+	private final PacketCoder coder;
+
+	private URI uri;
+	private Input input;
+	private Output output;
+
+	public CommunicatorBridge() throws URISyntaxException {
+		this("tcp://wsg50-00419251:1000");
+	}
+
+	public CommunicatorBridge(final String uri) throws URISyntaxException {
+		this(new URI(uri));
+	}
+	
+	public CommunicatorBridge(final URI uri) throws URISyntaxException {
+		coder = new PacketCoder();
+		setURI(uri);
+	}
+
+	@Override
+	public final void close() {
+		disconnect();
+	}
+
+	final void setIO(Input newInput, Output newOutput){
+		if(newInput == null){
+			throw new IllegalStateException("'newInput' is null");
+		}
+		if(newOutput == null){
+			throw new IllegalStateException("'newOutput' is null");
+		}
+		if (input != null) {
+			throw new IllegalStateException("'input' is already open");
+		}
+		if (output != null) {
+			throw new IllegalStateException("'output' is already open");
+		}
+		input = newInput;
+		output = newOutput;
+	}
+	
+	public void connect() throws IOException {
+		if(uri == null){
+			throw new IllegalArgumentException("'uri' is null");
+		}
+
+		final String scheme = uri.getScheme();
+		if("tcp".equals(scheme)){
+			connectTCP(uri.getHost(), uri.getPort());
+		}else{
+			throw new UnsupportedOperationException("unhandled scheme: " + scheme);
+		}
+	}
+
+	@SuppressWarnings("resource")
+	private void connectTCP(final String host, int port) throws IOException {
+		if(host == null){
+			throw new IllegalArgumentException("'host' is null");
+		}
+		if(port == -1){
+			port = 1000;
+		}
+		
+		IOException last = null;
+
+		for (final InetAddress address : InetAddress.getAllByName(host)) {
+			Socket socket = null;
+			Input in = null;
+			Output out = null;
+			try {
+				socket = new Socket(address, port);
+				out = new OutputToStream(socket.getOutputStream());
+				in = new InputFromStream(socket.getInputStream());
+			} catch (IOException ioe) {
+				last = ioe;
+
+				try{out.close();}catch(Exception e){}
+				try{socket.close();}catch(Exception e){}
+				continue;
+			}
+
+			setIO(in, out);
+			return;
+		}
+
+		throw last;
+	}
+
+	public void disconnect() {
+		try {
+			output.close();
+		} catch (Exception e) {
+		}
+		output = null;
+
+		try {
+			input.close();
+		} catch (Exception e) {
+		}
+		input = null;
+	}
+
+	
+
+	public final boolean isConnected() {
+		return (input != null) || (output != null);
+	}
+
+	public final Acknowledge readAcknowledge() {
+		if (input == null) {
+			throw new IllegalStateException("'input' is null");
+		}
+		synchronized (input) {
+			return coder.readAcknowledge(input, true);
+		}
+	}
+
+	public final void sendCommand(final Command command) {
+		if (command == null) {
+			throw new IllegalArgumentException("'command' is null");
+		}
+		if (output == null) {
+			throw new IllegalStateException("'output' is null");
+		}
+		synchronized (output) {
+			coder.write(output, command);
+		}
+	}
+
+	public void setURI(final URI newURI) throws URISyntaxException {
+		if (newURI == null) {
+			throw new IllegalArgumentException("'newURI' is null");
+		}
+		if (input != null) {
+			throw new IllegalStateException("'input' is already open");
+		}
+		uri = newURI;
+	}
+	
+	public final void setURI(final String newURI) throws URISyntaxException {
+		if (newURI == null) {
+			throw new IllegalArgumentException("'newURI' is null");
+		}
+		if (newURI.length() < 1) {
+			throw new IllegalArgumentException("'newURI' is empty");
+		}
+		setURI(new URI(newURI));
+	}
+}
